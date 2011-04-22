1d9b819c15eff0e86d14e653eb81f54f71ea787a
Christopher King <cwking@mindspring.com>
2011-04-22 02:02:52 -0700

    Commit rails bundle

---
+++ README.markdown
@@ -0,0 +1,2 @@
+This is RoR Chapter 3 creating Sample project.
+It uses static HTML, and simple Users and Microposts.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'b13034452e9d7e02bcec4dc2302ef019a085a079ec291832ba591c4548d30dbd1f99b9f264fcf433a61d863b42bfbe1b0715e66c653d4cbd2936bd2df16fa8c6'
