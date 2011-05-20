8a08d52d2f7030f9c8f2ab6097ba571401111d1f
Christopher King <cwking@mindspring.com>
2011-05-19 19:01:09 -0700

    Finished Sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,31 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
-		@title = "Sign Up"
+    @title = "Sign Up"
   end
 
-	def show
-		@user = User.find(params[:id])
+  def create
+
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+	 # Note that we are repeating ourselves here.
+	 # if we simple sent the new action then we get the title
+	 # from above, and it would render the new template.
+	 # why can't we do that? because we cannot generate a
+	 # new request.. we can only prepare response to create request
+      @title = "Sign up"
+      render 'new'
+    end
+
+  end
+
+  def show
+    @user = User.find(params[:id])
     @title = @user.name
-	end
+  end
 	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 SampleApp::Application.routes.draw do
-	resources :users
+  resources :users
 
-  match "/signup",		:to => "users#new"
-	
   get "pages/home"
-	match "/home",			:to => "pages#home"
+  match "/home",		:to => "pages#home"
 
-	match "/contact",		:to => "pages#contact"
-	match "/help",			:to => "pages#help"
-	match "/about",			:to => "pages#about"
-	root								:to => "pages#home"
+  match "/contact",	:to => "pages#contact"
+  match "/help",		:to => "pages#help"
+  match "/about",		:to => "pages#about"
+  root			:to => "pages#home"
 
-	end
+  match "/signup",	:to => "users#new"
+	
+end
