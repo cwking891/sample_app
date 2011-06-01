cc9f303b3ab9ad8cbda640091c180d4619ab80cb
Christopher King <cwking@mindspring.com>
2011-06-01 13:14:02 -0700

    refresh

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -128,7 +128,8 @@ describe User do
     begin
       user = User.create(@rec)
 	# allowed it
-	user.should eql(nil)
+	#user.should eql(nil)
+      user.should_not be_valid
     rescue ActiveRecord::RecordNotUnique => e #user is nil
 	# puts "Caught Exception " + e.class.to_s + " " +e.to_s
 	a.should be_valid
@@ -154,47 +155,5 @@ describe User do
       @user.should be_admin
     end
   end
-
-  describe "DELETE 'destroy'" do
-
-    before(:each) do
-      @user = Factory(:user)
-    end
-
-    describe "as a non-signed-in user" do
-      it "should deny access" do
-        delete :destroy, :id => @user
-        response.should redirect_to(signin_path)
-      end
-    end
-
-    describe "as a non-admin user" do
-      it "should protect the page" do
-        test_sign_in(@user)
-        delete :destroy, :id => @user
-        response.should redirect_to(root_path)
-      end
-    end
-
-    describe "as an admin user" do
-
-      before(:each) do
-        admin = Factory(:user, :email => "admin@example.com", :admin => true)
-        test_sign_in(admin)
-      end
-
-      it "should destroy the user" do
-        lambda do
-          delete :destroy, :id => @user
-        end.should change(User, :count).by(-1)
-      end
-
-      it "should redirect to the users page" do
-        delete :destroy, :id => @user
-        response.should redirect_to(users_path)
-      end
-    end
-  end
-
 
 end
